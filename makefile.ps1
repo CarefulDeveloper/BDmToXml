3494d53c5e750e22e1c35e34fb2fa7051a4d2175
November <lxk178230@qq.com>
2022-07-07 00:37:36 +0800

    修改项目结构

--- makefile.ps1
+++ makefile.ps1
@@ -2,7 +2,7 @@
 $codeList = Get-ChildItem | Where-Object {$_.Extension -eq ".cc" -or $_.Extension -eq ".c" -or $_.Extension -eq ".cpp" -and $_.BaseName -notmatch "test.*"}
 
 "正在编译以下文件："+ ($codeList -join ' ')
-g++.exe -fexec-charset=GBK -o BDmToXml.exe $codeList -I. -L. -lprotobuf -Os -s
+g++.exe -fexec-charset=GBK -o BDmToXml.exe $codeList -I./include -L./lib -lprotobuf -Os -s
 if($LASTEXITCODE -eq 0) {
     .\BDmToXml.exe .\seg.so .\res.txt
 }
